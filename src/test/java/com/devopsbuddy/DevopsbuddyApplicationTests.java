498d3760cda9fa61d75dbca34ddb60ca598fce59
Okan Bagci <bagciok@gmail.com>
2022-01-04 20:49:38 +0300

    Added i18n capability to the web skeleton

--- src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
+++ src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -1,13 +1,24 @@
 package com.devopsbuddy;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.devopsbuddy.web.i18n.I18NService;
 
 @SpringBootTest
 class DevopsbuddyApplicationTests {
 
+	@Autowired
+	private I18NService i18NService;
+
 	@Test
-	void contextLoads() {
+	public void testMessageByLocaleService() throws Exception {
+		String expectedResult = "Bootstrap starter template";
+		String messageId = "index.main.callout";
+		String actual = i18NService.getMessage(messageId);
+		assertEquals(expectedResult, actual, "The actual and expected Strings don't match");
 	}
 
 }
