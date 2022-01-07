69beffa23163d481eff7f5cf932bbcbce57cc599
Okan Bagci <bagciok@gmail.com>
2022-01-07 16:54:32 +0300

    Added the feedback journey code.

---
+++ src/main/java/com/devopsbuddy/backend/service/AbstractEmailService.java
@@ -0,0 +1,31 @@
+package com.devopsbuddy.backend.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+
+import com.devopsbuddy.web.domain.frontend.FeedbackPojo;
+
+public abstract class AbstractEmailService implements EmailService {
+
+	@Value("${default.to.address}")
+	private String defaultToAddress;
+	/**
+	 * Creates a Simple Mail message from a feedback pojo.
+	 * @param feedback the Feedback pojo
+	 * @return
+	 */
+	protected SimpleMailMessage prepareSimpleMailMessageFromFeedbackPojo(FeedbackPojo feedback) {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo(defaultToAddress);
+		message.setFrom(feedback.getEmail());
+		message.setSubject("[DevOps Buddy]: Feedback received from " + feedback.getFirstName() + " " + feedback.getLastName() + "!");
+		message.setText(feedback.getFeedback());
+		return message;
+	}
+	
+	@Override
+	public void sendFeedbackEmail(FeedbackPojo feedbackPojo) {
+		sendGenericEmail(prepareSimpleMailMessageFromFeedbackPojo(feedbackPojo));
+	}
+	
+}
---
+++ src/main/java/com/devopsbuddy/backend/service/EmailService.java
@@ -0,0 +1,20 @@
+package com.devopsbuddy.backend.service;
+
+import org.springframework.mail.SimpleMailMessage;
+
+import com.devopsbuddy.web.domain.frontend.FeedbackPojo;
+
+public interface EmailService {
+
+	/**
+	 * Sends an email with the content in the Feedback Pojo.
+	 * @param feedbackPojo the feedback Pojo
+	 */
+	public void sendFeedbackEmail(FeedbackPojo feedbackPojo);
+	
+	/**
+	 * Sends an email with the content of the Simple Mail Message object
+	 * @param message The object containing the email content
+	 */
+	public void sendGenericEmail(SimpleMailMessage message);
+}
